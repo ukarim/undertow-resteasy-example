a1cb22046880d0ff330d418f5f3331d4b4178572
ukarim <248163264@bk.ru>
2017-01-16 13:26:32 +0600

    add static serve

--- src/main/java/kz/ukarim/projects/example/Server.java
+++ src/main/java/kz/ukarim/projects/example/Server.java
@@ -1,11 +1,14 @@
 package kz.ukarim.projects.example;
 
 import io.undertow.Undertow;
+import io.undertow.server.handlers.resource.ClassPathResourceManager;
 import io.undertow.servlet.Servlets;
 import io.undertow.servlet.api.DeploymentInfo;
 import kz.ukarim.projects.example.api.ExampleApplication;
 import org.jboss.resteasy.plugins.server.undertow.UndertowJaxrsServer;
 import org.jboss.resteasy.spi.ResteasyDeployment;
+
+import static io.undertow.Handlers.resource;
 
 public class Server {
 
@@ -25,6 +28,10 @@ public class Server {
         deploymentInfo.addListener(Servlets.listener(org.jboss.weld.environment.servlet.Listener.class));
 
         server.deploy(deploymentInfo);
+
+        server.addResourcePrefixPath("/",
+                resource(new ClassPathResourceManager(Server.class.getClassLoader()))
+                        .addWelcomeFiles("index.html"));
 
         Undertow.Builder builder = Undertow.builder()
                 .addHttpListener(8080, "localhost");
